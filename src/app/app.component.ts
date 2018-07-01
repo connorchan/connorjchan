6eb4181438819f20901ec5dbf612bb3b5aa49479
Connor Chan <cchan2@conncoll.edu>
2018-07-01 14:10:50 -0400

    Fix Firebase error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  constructor() {
+    localStorage.removeItem('firebase:previous_websocket_failure');
+  }
 }
